086fec3a3a02fa4017ab55e05bf86dea4a5a8b7a
jeck5iv <Jeck-5.ivan@yandex.ru>
2025-03-24 05:47:27 +0300

    .

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,8 +1,46 @@
 package parser
 
-import "strings"
+import (
+	"strings"
+)
 
-// ParseCommand разбирает входную строку и возвращает команду и аргументы
-func ParseCommand(input string) []string {
-	return strings.Fields(input)
+// Parse разбирает команду, учитывая кавычки.
+func Parse(input string) []string {
+	var tokens []string
+	var current strings.Builder
+	inQuotes := false
+	quoteChar := byte(0)
+
+	for i := 0; i < len(input); i++ {
+		char := input[i]
+
+		if char == '"' || char == '\'' {
+			if inQuotes {
+				if char == quoteChar {
+					inQuotes = false
+					continue
+				}
+			} else {
+				inQuotes = true
+				quoteChar = char
+				continue
+			}
+		}
+
+		if char == ' ' && !inQuotes {
+			if current.Len() > 0 {
+				tokens = append(tokens, current.String())
+				current.Reset()
+			}
+			continue
+		}
+
+		current.WriteByte(char)
+	}
+
+	if current.Len() > 0 {
+		tokens = append(tokens, current.String())
+	}
+
+	return tokens
 }
